5918554ecc62a2c1afc5abc2dfd3520cc8380629
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-01-02 14:18:32 -0600

    removed useref import to pass build

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react"
+import React, { useState } from "react"
 import firebase from 'firebase'
 import { firestore, auth } from '../firebase'
 import { useCollectionData } from "react-firebase-hooks/firestore"
