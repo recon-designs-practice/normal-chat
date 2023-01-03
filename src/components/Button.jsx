eb8ab2e321d217496a306446eb3930e41e62b1d0
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-01-02 23:21:21 -0600

    added emotion to chatroom component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,25 @@
 import React from "react"
+import styled from '@emotion/styled'
 
-export default function Button({ onclick, children }) {
+const ButtonEl = styled.button`
+  background-color: #282c34;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1.25rem;
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`
+
+export default function Button({ onclick, className, children }) {
   return (
-    <button onClick={onclick}>{children}</button>
+    <ButtonEl className={className} onClick={onclick}>{children}</ButtonEl>
   )
 }
--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,18 +1,44 @@
 import React, { useState } from "react"
-import firebase from 'firebase'
-import { firestore, auth } from '../firebase'
+import styled from "@emotion/styled"
+import firebase from "firebase"
+import { firestore, auth } from "../firebase"
 import { useCollectionData } from "react-firebase-hooks/firestore"
-import ChatMessage from '../components/ChatMessage'
-import Button from '../components/Button'
+import ChatMessage from "../components/ChatMessage"
+import Button from "../components/Button"
+
+const Main = styled.main`
+  padding: 10px;
+  height: 80vh;
+  margin: 10vh 0 10vh;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+`
+
+const Form = styled.form`
+  height: 10vh;
+  position: fixed;
+  bottom: 0;
+  background-color: rgb(24, 23, 23);
+  width: 100%;
+  max-width: 728px;
+  display: flex;
+  font-size: 1.5rem;
+`
+
+const FormButton = styled(Button)`
+  width: 20%;
+  background-color: rgb(56, 56, 143);
+`
 
 export default function Chatroom() {
-  const messagesRef = firestore.collection('messages')
-  const query = messagesRef.orderBy('createdAt').limit(50)
-  const [messages] = useCollectionData(query, { idField: 'id' })
-  const [formValue, setFormValue] = useState('')
+  const messagesRef = firestore.collection("messages")
+  const query = messagesRef.orderBy("createdAt").limit(50)
+  const [messages] = useCollectionData(query, { idField: "id" })
+  const [formValue, setFormValue] = useState("")
   // const dummy = useRef()
 
-  const sendMessage = async(e) => {
+  const sendMessage = async (e) => {
     e.preventDefault()
 
     const { uid, photoURL } = auth.currentUser
@@ -21,30 +47,30 @@ export default function Chatroom() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
+      photoURL,
     })
 
-    setFormValue('')
+    setFormValue("")
 
     // dummy.current.scrollIntoView({ behavior: 'smooth' })
   }
 
   return (
     <>
-      <main>
-        {
-          messages && messages.map((msg) => {
-            return (
-              <ChatMessage key={msg.id}>{msg.text}</ChatMessage>
-            )
-          })
-        }
-      </main>
+      <Main>
+        {messages &&
+          messages.map((msg) => {
+            return <ChatMessage key={msg.id}>{msg.text}</ChatMessage>
+          })}
+      </Main>
 
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
-        <Button>S</Button>
-      </form>
+      <Form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <FormButton>🕊️</FormButton>
+      </Form>
     </>
   )
 }
