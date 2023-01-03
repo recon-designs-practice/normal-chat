6291cd2e380dae9a819ca38ed8319d07ebaa890b
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-01-02 23:38:50 -0600

    added emotion to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import firebase from 'firebase'
+import React from "react"
+import styled from "@emotion/styled"
+import firebase from "firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
-import { auth } from './firebase'
-import Header from './components/Header'
-import Chatroom from './components/Chatroom'
-import Button from './components/Button'
-import './App.css';
+import { auth } from "./firebase"
+import Header from "./components/Header"
+import Chatroom from "./components/Chatroom"
+import Button from "./components/Button"
+import "./App.css"
+
+const AppContainer = styled.div`
+  text-align: center;
+  max-width: 728px;
+  margin: 0 auto;
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: rgb(40, 37, 53);
+`
 
 function signIn() {
   const provider = new firebase.auth.GoogleAuthProvider()
@@ -17,13 +32,13 @@ function App() {
   const [user] = useAuthState(auth)
 
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
-      <section>
+      <Section>
         {user ? <Chatroom /> : <Button onclick={signIn}>Log in</Button>}
-      </section>
-    </div>
-  );
+      </Section>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
