a7c7564084d1a199d162a68dd7f64e1d639f84db
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-01-04 01:31:07 -0600

    added all routes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
 import React from "react"
-import { Switch, Route } from 'react-router-dom'
-import firebase from "firebase"
-import { useAuthState } from "react-firebase-hooks/auth"
-import { auth } from "./firebase"
-import styled from "@emotion/styled"
-import LandingPage from './pages/Landing'
-import SignupPage from './pages/Signup'
-import ChatPage from './pages/Chat'
-import Header from "./components/Header"
-import Chatroom from "./components/Chatroom"
-import Button from "./components/Button"
+import { Switch, Route } from "react-router-dom"
+import LandingPage from "./pages/Landing"
+import SignupPage from "./pages/Signup"
+import ChatPage from "./pages/Chat"
 import "./App.css"
 
-const AppContainer = styled.div`
-  text-align: center;
-  max-width: 728px;
-  margin: 0 auto;
-`
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 100vh;
-  background-color: rgb(40, 37, 53);
-`
-
-function signIn() {
-  const provider = new firebase.auth.GoogleAuthProvider()
-
-  auth.signInWithPopup(provider)
-}
-
 function App() {
-  const [user] = useAuthState(auth)
-
   return (
     <>
       <Switch>
-      <Route path={'/chat'}>
+        <Route path={"/chat"}>
           <ChatPage />
         </Route>
-        <Route path={'/signup'}>
+        <Route path={"/signup"}>
           <SignupPage />
         </Route>
-        <Route path={'/'}>
+        <Route path={"/"}>
           <LandingPage />
         </Route>
       </Switch>
     </>
-    // <AppContainer>
-    //   <Header />
-    //   <Section>
-    //     {user ? <Chatroom /> : <Button onclick={signIn}>Log in</Button>}
-    //   </Section>
-    // </AppContainer>
   )
 }
 
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react"
+import firebase from "firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../firebase"
+import styled from "@emotion/styled"
+import Header from "../components/Header"
+import Chatroom from "../components/Chatroom"
+import Button from "../components/Button"
+
+const AppContainer = styled.div`
+  text-align: center;
+  max-width: 728px;
+  margin: 0 auto;
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: rgb(40, 37, 53);
+`
+
+function signIn() {
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  auth.signInWithPopup(provider)
+}
 
 export default function ChatPage() {
+  const [user] = useAuthState(auth)
+
   return (
-    <div>Chat page</div>
+    <AppContainer>
+      <Header />
+      <Section>
+        {user ? <Chatroom /> : <Button onclick={signIn}>Log in</Button>}
+      </Section>
+    </AppContainer>
   )
 }
