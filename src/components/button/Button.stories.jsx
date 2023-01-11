910c37a864df3c4be97ac9aaa97f92bcd69d8ce4
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-01-10 23:17:12 -0600

    removed uneeded storybook files and created basic button story

---
+++ src/components/button/Button.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Button from "./Button"
+
+export const Primary = () => <Button>Click me</Button>
+
+export default {
+  title: 'Components/Button',
+  component: Button
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Header } from './Header'
-export { default as Button } from './Button'
+// export { default as Button } from './Button'
+export { Button } from './button'
 export { default as Input } from './Input'
 export { default as Form } from './Form'
 export { default as Chatroom } from './Chatroom'
